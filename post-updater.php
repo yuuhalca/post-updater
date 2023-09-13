7b0ec295c3801531ca22f764cfd79aa28fd4166f
石賀悠 <ishigayuu@ishigayuunoiMac-7.local>
2023-09-13 09:57:08 +0900

    廃盤情報を抜粋に追加

--- post-updater.php
+++ post-updater.php
@@ -385,6 +385,13 @@ function MD_BlogDo()
 
 
         /**
+         * 期中に廃盤
+         */
+        if($value[2] == "2200/12/31" && $value[1] == 0){
+            $haiban = "<strong>※当商品は製造終了に伴い、現在取扱店様にございます在庫にて廃盤となりました。</strong><br>";
+        }
+
+        /**
          * Description加工開始
          */
         $postcontent = "";
@@ -949,9 +956,9 @@ function MD_BlogDo()
         if (SYNC_DESCRIPTION === true) {
             if ($simpleorvariable[$data_final[$value[0]]["ID"]][0] == "product_variation") {
                 $update[] = $db->updateDatabase("wp_posts", "", "post_excerpt", $data_final[$value[0]]["ID"], "ID");
-                $update[] = $db->updateDatabase("wp_posts", "'" . $value[7] . "'", "post_excerpt", $simpleorvariable[$data_final[$value[0]]["ID"]][1], "ID");
+                $update[] = $db->updateDatabase("wp_posts", "'" . $haiban . $value[7] . "'", "post_excerpt", $simpleorvariable[$data_final[$value[0]]["ID"]][1], "ID");
             } else {
-                $update[] = $db->updateDatabase("wp_posts", "'" . $value[7] . $postcontent . "'", "post_excerpt", $data_final[$value[0]]["ID"], "ID");
+                $update[] = $db->updateDatabase("wp_posts", "'" . $haiban . $value[7] . $postcontent . "'", "post_excerpt", $data_final[$value[0]]["ID"], "ID");
             }
             if (SYNC_PRICE === true) {
                 $update[] = $db->updateDatabase("wp_postmeta", "'" . $value[39] . "'", "meta_value", [$data_final[$value[0]]["ID"], "_price"], ["post_id", "meta_key"]);
