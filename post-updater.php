5e22d903a1f9f7d1bdc82d5829dd21ded6e73366
yuuhalca <h.c.nicol@gmail.com>
2024-04-04 15:33:29 +0900

    commit

--- post-updater.php
+++ post-updater.php
@@ -310,7 +310,7 @@ function MD_BlogDo()
     $debug = "";
     foreach($csv_data as $value){
         foreach($value as $val){
-            if(strpos($val,"㎝") !== false){
+            if(strpos($val, "32×7.5×6.5") !== false){
                 $debug .= $special_cara->replaceMachineChar($val)."\n";
             }
         }
